6d3e1bd1acfd1dee19623b424cc4636b61032dfe
sharas <apkrovos@gmail.com>
2019-05-19 22:23:45 +0000

    changed port for auth proxy to 8080 to avoid clashes. Fixed webapi test for todays menus to query todays_menus

--- code/tests/test_webapi.py
+++ code/tests/test_webapi.py
@@ -4,10 +4,12 @@ import subprocess
 import urllib
 
 
-API_URL = 'http://localhost/flyers'
+HOST_URL = 'http://localhost:8080/'
+FLYERS_URL = HOST_URL + 'flyers'
+TODAYS_URL = HOST_URL + 'todays_menus'
 
-def format_url_query(params: dict) -> str:
-    return API_URL + '?' + urllib.parse.urlencode(params)
+def format_url_query(params: dict, url=FLYERS_URL) -> str:
+    return FLYERS_URL + '?' + urllib.parse.urlencode(params)
 
 def setup_module():
     import time
@@ -18,32 +20,32 @@ def teardown_function():
     subprocess.run(['docker-compose', 'restart', 'db'], check=True)
 
 def test_can_list_flyers_without_authentication():
-    r = requests.get(API_URL)
+    r = requests.get(FLYERS_URL)
     assert r.status_code == 200
 
 def test_cant_create_flyer_without_authentication():
     flyer = {'restaurant_name': 'restaurant 1', 'date': '2019-04-11', 'menu': 'kokletai, pyragas'} 
-    r = requests.put(API_URL, json = flyer) 
+    r = requests.put(FLYERS_URL, json = flyer) 
     assert r.status_code == 401
 
 def test_cant_create_flyer_with_partial_data():
     restaurant_auth = ('restaurant1', 'pass1') 
     partial_flyer = {'restaurant_name': 'restaurant 1', 'menu': 'kokletai, pyragas'} 
-    r = requests.put(API_URL, json = partial_flyer, auth = restaurant_auth) 
+    r = requests.put(FLYERS_URL, json = partial_flyer, auth = restaurant_auth) 
     assert r.status_code == 400
     assert 'Invalid flyer' in r.json()['description']
 
 def populate_menus(menus):
     for item in menus:
         auth, menu = item
-        r = requests.put(API_URL, json = menu, auth=auth) 
+        r = requests.put(FLYERS_URL, json = menu, auth=auth) 
         assert r.status_code == 200
 
 def test_added_flyer_found():
     flyer = {'restaurant_name': 'best restaurant', 'date': '2019-04-11', 'menu': 'kokletai, pyragas'}
     restaurant_id = 'restaurant1'
     populate_menus([((restaurant_id, 'pass1'), flyer)])
-    r = requests.get(API_URL)
+    r = requests.get(FLYERS_URL)
     assert r.status_code == 200
     query_result = r.json()
     assert query_result == [dict(flyer, _id = restaurant_id)]
@@ -71,7 +73,7 @@ def test_todays_menus_finds_all_todays_menus():
                     (('restaurant3', 'pass3'), {'restaurant_name': "best restaurant 3", 'date' : today, 'menu' : 'geresni kokletai, kompotas'}),
                     (('restaurant4', 'pass4'), {'restaurant_name': "best restaurant 4", 'date' : today, 'menu' : 'geresni kokletai, kompotas'}),
                     (('restaurant5', 'pass5'), {'restaurant_name': "best restaurant 5", 'date' : today, 'menu' : 'geresni kokletai, kompotas'})])
-    r = requests.get(format_url_query(dict(date=today)))
+    r = requests.get(format_url_query(dict(date=today), url=TODAYS_URL))
     assert r.status_code == 200
     query_result = r.json()
     assert len(query_result) == 3
